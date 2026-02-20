1c2599966d94e07f1de6df954a735cbbe97c536d
João Vitor S Xavier <sedlak.xavier@escola.pr.gov.br>
2026-02-20 08:42:40 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,12 @@
+nome = "João"
+idade = 16
+altura = 1.73
+genero = "masculino"
+cor_de_pele = "branco"
+cor_de_cabelo = "preto"
+tamanho_cabelo = "curto"
+estilo_cabelo = "degradê baixo"
+curso= "DS"
+hobby= "jogar jogos digitais"
+
+print(f"Olá, eu sou o {nome}, tenho {idade} anos, {altura:.2f}m, sou {genero}, pele {cor_de_pele}, cabelo {cor_de_cabelo} {tamanho_cabelo} no estilo {estilo_cabelo}, faço {curso} e adoro {hobby}.")
